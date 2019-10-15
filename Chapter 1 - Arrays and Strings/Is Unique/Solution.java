4ea73f41f1e733d98fa2e69988ea038919c91e80
Diogo Barbosa <pessoal.dbarbosa@gmail.com>
2019-10-15 21:10:35 +0100

    Added chapter 1

--- Chapter 1 - Arrays and Strings/Is Unique/Solution.java
+++ Chapter 1 - Arrays and Strings/Is Unique/Solution.java
@@ -3,11 +3,15 @@ import java.util.*;
 public class Solution {
 
     public static boolean isUnique(String s) {
-        char[] characters = s.toCharArray();
-        Arrays.sort(characters);
-        for (int i = 0; i < characters.length - 1; i++)
-            if (characters[i] == characters[i + 1])
+        if (s.length() > 256)
+            return false;
+        int stored = 0;
+        for (int i = 0; i < str.length(); i++) {
+            int val = str.charAt(i);
+            if ((stored & (1 << val)) > 0)
                 return false;
+            stored |= (1 << val);
+        }
         return true;
     }
 
